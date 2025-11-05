9c77f01ee0947b429fc581eaba7b46f0967a83cf
Teja Coding <dropteja@gmail.com>
2025-11-05 19:22:11 +0530

    deleted here

--- 11_tfvars_mult_env/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  common_name = "${var.project}-${var.environment}"
-  common_tags = {
-    Project = var.project
-    Terraform = "true"
-  }
-}
--- 11_tfvars_mult_env/prod/backend.tf
+++
@@ -1,5 +0,0 @@
-bucket = "fineshit-prod"
-key    = "tfvars-multi-env-demo"
-region = "us-east-1"
-use_lockfile = true
-encrypt = true
