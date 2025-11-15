879d0c7cf3876adeedead3de06175ee2f07bf1cd
Teja Coding <dropteja@gmail.com>
2025-11-15 15:31:26 +0530

    done

--- 15_roboshop_dev_infra/40-databases/main.tf
+++ 15_roboshop_dev_infra/40-databases/main.tf
@@ -33,49 +33,49 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [ 
     "chmod +x /tmp/bootstrap.sh",
-     "sudo sh /tmp/bootstrap.sh"
+     "sudo sh /tmp/bootstrap.sh mongodb"
      ]
   }
 }
 
 #----------------------------------------------------------
 
-# resource "aws_instance" "redis" {
-#   ami = local.ami_id
-#   instance_type = "t3.micro"
-#   vpc_security_group_ids = [local.redis_sg_id]
-#   subnet_id = local.database_subnet_ids
+resource "aws_instance" "redis" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_ids
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${local.common_name_suffix}-redis"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-redis"
+    }
+  )
+}
 
-# resource "terraform_data" "redis" {
-#   triggers_replace = [
-#     aws_instance.redis.id
-#   ]
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
 
-#   connection {
-#     type = "ssh"
-#     user = "ec2-user"
-#     password = "DevOps321"
-#     host = aws_instance.redis.private_ip
-#   }
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+  }
 
-# #terraform copies this file to redis server
-#   provisioner "file" {
-#     source = "bootstrap.sh" 
-#     destination = "/tmp/bootstrap.sh"
-#   }
+#terraform copies this file to redis server
+  provisioner "file" {
+    source = "bootstrap.sh" 
+    destination = "/tmp/bootstrap.sh"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [ 
-#     "chmod +x /tmp/bootstrap.sh",
-#      "sudo sh /tmp/bootstrap.sh redis"
-#      ]
-#   }
-# }
+  provisioner "remote-exec" {
+    inline = [ 
+    "chmod +x /tmp/bootstrap.sh",
+     "sudo sh /tmp/bootstrap.sh redis redis"
+     ]
+  }
+}
