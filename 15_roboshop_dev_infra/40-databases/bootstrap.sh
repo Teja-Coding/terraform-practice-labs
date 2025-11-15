3ec30cccff6953033014c6aa15e7e741b6939030
Teja Coding <dropteja@gmail.com>
2025-11-15 13:00:05 +0530

    done

--- 15_roboshop_dev_infra/40-databases/bootstrap.sh
+++ 15_roboshop_dev_infra/40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -u https://github.com/Teja-Coding/ansible-roboshop-roles-tf.git -e commponent=mongodb main.yaml
+ansible-pull -u https://github.com/Teja-Coding/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
