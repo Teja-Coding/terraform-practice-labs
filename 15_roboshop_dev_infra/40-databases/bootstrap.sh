16a7f980806cecf7aa146192b8c195a549ad53fa
Teja Coding <dropteja@gmail.com>
2025-11-16 20:03:37 +0530

    done bootstrap fine change

--- 15_roboshop_dev_infra/40-databases/bootstrap.sh
+++ 15_roboshop_dev_infra/40-databases/bootstrap.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-component=$1
+
+# ansible-pull -U https://github.com/Teja-Coding/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
 dnf install ansible -y
-ansible-pull -U https://github.com/Teja-Coding/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+REPO_URL=https://github.com/Teja-Coding/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+component=$1
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
+cd $REPO_DIR
+
+#check if ansible repo is already cloned or not
+if[ -d $ANSIBLE_DIR]; then
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e component=$component main.yaml
+
