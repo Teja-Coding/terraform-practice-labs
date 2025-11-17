c8a017b1d8a6cd472c9b3a06dff6ab8cbbc9d7f6
Teja Coding <dropteja@gmail.com>
2025-11-17 18:22:00 +0530

    route53

--- 15_roboshop_dev_infra/40-databases/variables.tf
+++ 15_roboshop_dev_infra/40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z01877457DKZDKQFO78G"
+}
+
+variable "domain_name" {
+  default = "fineshit.shop"
+}
