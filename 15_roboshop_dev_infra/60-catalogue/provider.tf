aee533461eef8b5333ec4e6265d14ee7d0d9a788
Teja Coding <dropteja@gmail.com>
2025-11-17 18:28:32 +0530

    done

---
+++ 15_roboshop_dev_infra/60-catalogue/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+    backend "s3" {
+    bucket = "fineshit"
+    key    = "catalogue_service"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+  
+}
+
+provider "aws" {
+  # Configuration options
+    region = "us-east-1"
+}
