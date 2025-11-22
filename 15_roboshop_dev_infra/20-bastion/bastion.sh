cb8874ff296aac30488521d7c1386c29e9744c04
Teja Coding <dropteja@gmail.com>
2025-11-22 15:43:32 +0530

    terraform

--- 15_roboshop_dev_infra/20-bastion/bastion.sh
+++ 15_roboshop_dev_infra/20-bastion/bastion.sh
@@ -5,3 +5,8 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 
 sudo yum -y install terraform
+
+# growing the /home volume for terraform purpose
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
--- 15_roboshop_dev_infra/60-catalogue/locals.tf
+++ 15_roboshop_dev_infra/60-catalogue/locals.tf
@@ -2,7 +2,10 @@ locals {
   ami_id = data.aws_ami.data_source.id
   common_name_suffix = "${var.project_name}-${var.environment}"
   catalogue_sg_id  = data.aws_ssm_parameter.catalogue_sg_id.value
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   private_subnet_ids = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids_all = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
+
 
 
   common_tags  = {
