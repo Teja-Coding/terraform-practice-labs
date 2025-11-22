47495d4062d7b440bc95ab60e0b60cdc5a6c457e
Teja Coding <dropteja@gmail.com>
2025-11-22 18:10:09 +0530

    done

--- 15_roboshop_dev_infra/50-backend_alb/varibale.tf
+++ 15_roboshop_dev_infra/50-backend_alb/varibale.tf
@@ -6,3 +6,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z01877457DKZDKQFO78G"
+}
+
+variable "domain_name" {
+  default = "fineshit.shop"
+}
